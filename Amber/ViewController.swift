9d68581a51ee71b27954a02868b14af501525f22
Giancarlo Buenaflor <giancarlo_buenaflor@yahoo.com>
2018-10-30 22:18:41 +0100

    Initial Commit

---
+++ Amber/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Amber
+//
+//  Created by Giancarlo Buenaflor on 30.10.18.
+//  Copyright © 2018 Giancarlo Buenaflor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
